4a5ade401c64382b0f84e9a610d02cd779e72660
Zaharova Ludmila <StoneFox1992@yandex.ru>
2022-01-25 20:59:33 +0400

    Мой первый коммит!

---
+++ Praktikum.java
@@ -0,0 +1,13 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 9;
+        a += 1;
+        a *= 5;
+        b /= 3;
+
+        int result = a - b;
+
+        System.out.println(result);
+    }
+}
